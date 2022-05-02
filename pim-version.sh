75c53ccd991cf9e5ce63a0efa086c89b64a2c657
Greg Hesla <g.hesla@mumms.com>
2022-05-02 11:00:01 -0400

    chore: update pim-version for better user info display

--- pim-version.sh
+++ pim-version.sh
@@ -22,10 +22,18 @@ if [ -z ${NEWVER} ]; then
   echo "ERROR: a new version is required"
   exit 1;
 fi
+echo -e "\nUpdating PIM version to ${NEWVER}"
 
+echo -e "\n  Updating PIM version in hbgwt/HBDynaWeb/build.xml:"
+echo "    ...before:"
 grep hbgwt/HBDynaWeb/build.xml -e "property name=\"MinorVersion\""
 sed -i '' "s/property name=\"MinorVersion\" value=\".*\"/property name=\"MinorVersion\" value=\"${NEWVER}\"/" hbgwt/HBDynaWeb/build.xml
+echo "    ...after:"
 grep hbgwt/HBDynaWeb/build.xml -e "property name=\"MinorVersion\""
+
+echo -e "\n  Updating PIM version in hbgwt/HBUnitTest/pom.xml:"
+echo "    ...before:"
 grep hbgwt/HBUnitTest/pom.xml -e "<version>2\..*-SNAPSHOT</version>"
 sed -E -i '' "s/<version>(.*)\..*-SNAPSHOT<\/version>/<version>\1.${NEWVER}-SNAPSHOT<\/version>/" hbgwt/HBUnitTest/pom.xml
+echo "    ...after:"
 grep hbgwt/HBUnitTest/pom.xml -e "<version>2\..*-SNAPSHOT</version>"
