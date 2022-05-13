7d0769477fc82dff94b8277be469e0bccd9ae9d0
Greg Hesla <g.hesla@mumms.com>
2022-05-13 11:13:16 -0400

    chore: process major and minor version numbers explicitly in build.xml

--- pim-version.sh
+++ pim-version.sh
@@ -17,6 +17,8 @@ IFS=$'\n\t'
 #####
 
 NEWVER=${1}
+MAJOR=$(echo "${NEWVER}" | cut -d . -f 1)
+MINOR=$(echo "${NEWVER}" | cut -d . -f 2)
 
 if [ -z ${NEWVER} ]; then
   echo "ERROR: a new version is required"
@@ -25,15 +27,24 @@ fi
 echo -e "\nUpdating PIM version to ${NEWVER}"
 
 echo -e "\n  Updating PIM version in hbgwt/HBDynaWeb/build.xml:"
-echo "    ...before:"
+echo "    ...major version before:"
+grep hbgwt/HBDynaWeb/build.xml -e "property name=\"MajorVersion\""
+echo "    ...minor version before:"
 grep hbgwt/HBDynaWeb/build.xml -e "property name=\"MinorVersion\""
-sed -i '' "s/property name=\"MinorVersion\" value=\".*\"/property name=\"MinorVersion\" value=\"${NEWVER}\"/" hbgwt/HBDynaWeb/build.xml
-echo "    ...after:"
+
+sed -i '' "s/property name=\"MajorVersion\" value=\".*\"/property name=\"MajorVersion\" value=\"${MAJOR}\"/" hbgwt/HBDynaWeb/build.xml
+sed -i '' "s/property name=\"MinorVersion\" value=\".*\"/property name=\"MinorVersion\" value=\"${MINOR}\"/" hbgwt/HBDynaWeb/build.xml
+
+echo -e "\n    ...major version after:"
+grep hbgwt/HBDynaWeb/build.xml -e "property name=\"MajorVersion\""
+echo "    ...minor version after:"
 grep hbgwt/HBDynaWeb/build.xml -e "property name=\"MinorVersion\""
 
 echo -e "\n  Updating PIM version in hbgwt/HBUnitTest/pom.xml:"
 echo "    ...before:"
 grep hbgwt/HBUnitTest/pom.xml -e "<version>.*-SNAPSHOT</version>"
+
 sed -E -i '' "s/<version>.*-SNAPSHOT<\/version>/<version>${NEWVER}-SNAPSHOT<\/version>/" hbgwt/HBUnitTest/pom.xml
+
 echo "    ...after:"
 grep hbgwt/HBUnitTest/pom.xml -e "<version>.*-SNAPSHOT</version>"
