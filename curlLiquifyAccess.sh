c66ee3cf52ab5945acaf52ecd03bb771ff348e73
Greg Hesla <g.hesla@mumms.com>
2022-07-19 13:54:57 -0400

    fix: fixed path to hummingbird-auth-sso...jar

--- curlLiquifyAccess.sh
+++ curlLiquifyAccess.sh
@@ -6,7 +6,7 @@ TENANT=$1
 HOST="https://access.gca-prod.mumms.com"
 API_PATH="/liquifydatabase?tenant=$1"
 JWT_SECRET=UUFj2UEJZ2D9GqdkL99h2sVpP4sjrzD7xzzeywPNUxxPwKCBd7UXzvv3KEVTnuLC
-JWT_TOKEN=`java -jar ./hummingbird-auth-sso-1.0.7-RELEASE-jar-with-dependencies.jar -b $1 -s $JWT_SECRET`
+JWT_TOKEN=`java -jar ../lib/hummingbird-auth-sso-1.0.7-RELEASE-jar-with-dependencies.jar -b $1 -s $JWT_SECRET`
 REQUEST="$HOST/$API_PATH"
 curl -X GET \
 	--header "Authorization: Bearer $JWT_TOKEN" \
